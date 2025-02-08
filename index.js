2d59642ba3f5bd0802480761191a57d4ccf709e9
rohank15 <rohanrk124@gmail.com>
2025-02-09 04:03:21 +0530

    .

---
+++ index.js
@@ -0,0 +1,36 @@
+function updateDisplay(value)
+{
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay()
+{
+    document.getElementById("display").value = "";
+}
+
+function remSingleDigit()
+{
+    let display = document.getElementById("display")
+    display.value = display.value.slice(0,-1)
+}
+
+function calculate()
+{
+    let display = document.getElementById("display")
+
+    if(display.value === "")
+    {
+        display.value = ""
+        return
+    }
+
+    try
+    {
+        display.value = eval(display.value)
+    }
+    catch(error)
+    {
+        display.value = ""
+    }
+   
+}
