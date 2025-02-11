d15af3d54e96b7b191b2a29e1818f48e05f8c124
rohank15 <rohanrk124@gmail.com>
2025-02-11 18:45:59 +0530

    added keyboard inputs

--- index.js
+++ index.js
@@ -41,3 +41,103 @@ function calculate()
         display.value = ""
     }
 }
+
+function selectKey(x)
+{
+    switch(x)
+    {
+        case 'Backspace':
+            remSingleDigit()
+            break
+            
+        case '(':
+            updateDisplay('(')
+            break
+    
+        case ')':
+            updateDisplay(')')
+            break    
+
+        case '%':
+            updateDisplay('%')
+            break
+    
+        case '7':
+            updateDisplay('7')
+            break  
+            
+        case '8':
+            updateDisplay('8')
+            break
+    
+        case '9':
+            updateDisplay('9')
+            break    
+
+        case 'x':
+            updateDisplay('*')
+            break
+    
+        case '*':
+            updateDisplay('*')
+            break 
+
+        case '4':
+            updateDisplay('4')
+            break
+    
+        case '5':
+            updateDisplay('5')
+            break    
+
+        case '6':
+            updateDisplay('6')
+            break
+    
+        case '-':
+            updateDisplay('-')
+            break  
+            
+        case '1':
+            updateDisplay('1')
+            break
+    
+        case '2':
+            updateDisplay('2')
+            break    
+
+        case '3':
+            updateDisplay('3')
+            break
+    
+        case '+':
+            updateDisplay('+')
+            break 
+
+        case '0':
+            updateDisplay('0')
+            break  
+            
+        case '.':
+            updateDisplay('.')
+            break
+    
+        case 'Delete':
+            remSingleDigit()
+            break    
+
+        case '/':
+            updateDisplay('/')
+            break
+    
+        case 'Enter':
+            calculate()
+            break 
+    }
+
+    return true
+}
+
+document.addEventListener("keydown", function(event){
+    selectKey(event.key)
+    })
